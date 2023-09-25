f48ee8aca7b467a246565ad09879ec43f5d1750b
YuriyLoginov <loginov.yur@gmail.com>
2023-09-25 22:25:40 +0400

    Update

--- src/main/java/com/snwm/englishbot/service/UserService.java
+++ src/main/java/com/snwm/englishbot/service/UserService.java
@@ -1,15 +1,7 @@
 package com.snwm.englishbot.service;
 
 import com.snwm.englishbot.entity.User;
-import com.snwm.englishbot.entity.Word;
-import com.snwm.englishbot.repository.UserRepository;
-import com.snwm.englishbot.repository.WordRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Message;
-
-import java.util.List;
-import java.util.Set;
 
 public interface UserService {
     User findUserByChatId(Long id);
