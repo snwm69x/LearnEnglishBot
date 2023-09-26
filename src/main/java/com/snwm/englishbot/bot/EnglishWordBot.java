4f136c30eae2bffed39432b847d51114eaeb702a
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-09-26 20:06:24 +0400

    пыль протёр

--- src/main/java/com/snwm/englishbot/bot/EnglishWordBot.java
+++ src/main/java/com/snwm/englishbot/bot/EnglishWordBot.java
@@ -3,10 +3,10 @@ package com.snwm.englishbot.bot;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
+import com.snwm.englishbot.entity.User;
 import com.snwm.englishbot.entity.Word;
 import com.snwm.englishbot.service.UserService;
 import com.snwm.englishbot.service.WordService;
@@ -18,13 +18,9 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -71,20 +67,23 @@ public class EnglishWordBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText() && !update.getMessage().getText().equals("/start")
+                && !update.getMessage().getText().equals("Новое слово")
+                && !update.getMessage().getText().equals("О боте")) {
+            handleUnknownCommand(update.getMessage());
+        }
         // Обработка первого сообщения пользователя
         if (update.hasMessage() && update.getMessage().hasText()) {
-            Message message = update.getMessage();
-            if (message.getText().equals("/start")) {
-                handleStartCommand(message);
+            if (update.getMessage().getText().equals("/start")) {
+                handleStartCommand(update.getMessage());
             }
             // Обработка команды "О боте"
-            if (message.getText().equals("О боте")) {
-                handleInfoCommand(message);
+            if (update.getMessage().getText().equals("О боте")) {
+                handleInfoCommand(update.getMessage());
             }
-
             // Обработка команды "Новое слово"
-            if (message.getText().equals("Новое слово")) {
-                handleNewWordCommand(message);
+            if (update.getMessage().getText().equals("Новое слово")) {
+                handleNewWordCommand(update.getMessage());     
             }
         }
     }
@@ -93,7 +92,7 @@ public class EnglishWordBot extends TelegramLongPollingBot {
         // Если кэш пуст, то заполняем его словами из базы данных.
         // но всё равно слова в ответах могут повторится
         List<Word> words = wordsCache.get(message.getChatId().toString());
-        if(words == null) {
+        if (words == null) {
             words = wordService.getAllWordsInDb();
             wordsCache.put(message.getChatId().toString(), words);
         }
@@ -101,7 +100,7 @@ public class EnglishWordBot extends TelegramLongPollingBot {
         wordsCache.get(message.getChatId().toString()).remove(word);
         List<String> options = new ArrayList<>();
         List<Word> tempWords = wordsCache.get(message.getChatId().toString());
-        for(int i = 0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
             int randomIndex = (int) (Math.random() * wordsCache.size());
             options.add(tempWords.get(randomIndex).getTranslation());
             tempWords.remove(randomIndex);
@@ -126,7 +125,10 @@ public class EnglishWordBot extends TelegramLongPollingBot {
     private void handleInfoCommand(Message message) {
         SendMessage infoMessage = new SendMessage();
         infoMessage.setChatId(message.getChatId().toString());
-        infoMessage.setText("Бот для изучения английского языка. Версия 1.0");
+        infoMessage.setText("Бот для изучения английского языка. Версия 0.1 \n" +
+                "Автор: - \n" +
+                "GitHub: - \n" +
+                "если бот не работает используйте /start");
         try {
             execute(infoMessage);
         } catch (TelegramApiException e) {
@@ -135,35 +137,49 @@ public class EnglishWordBot extends TelegramLongPollingBot {
     }
 
     private void handleStartCommand(Message message) {
-        if (userService.findUserByChatId(message.getChatId()) == null) {
-                    userService.createNewUser(message);
-                    wordService.setAllWord(message.getChatId());
-                }
+        User user = userService.findUserByChatId(message.getChatId());
+        if (user == null) {
+            userService.createNewUser(message);
+            wordService.setAllWord(message.getChatId());
+        }
+        if (user != null && user.getWords().size() == 0) {
+            wordService.setAllWord(message.getChatId());
+        }
+        SendMessage startMessage = new SendMessage();
+        startMessage.setChatId(message.getChatId().toString());
+        startMessage.setText("Привет, я бот для изучения английского языка. Выбери действие:");
+        // Создание клавиатуры
+        ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
+        markup.setResizeKeyboard(true);
+        markup.setOneTimeKeyboard(false);
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        // Добавление кнопок
+        KeyboardRow row1 = new KeyboardRow();
+        KeyboardButton button_wordplay = new KeyboardButton();
+        button_wordplay.setText("Новое слово");
+        row1.add(button_wordplay);
+        KeyboardButton button_info = new KeyboardButton();
+        button_info.setText("О боте");
+        row1.add(button_info);
+        keyboard.add(row1);
+        markup.setKeyboard(keyboard);
+        startMessage.setReplyMarkup(markup);
+        try {
+            execute(startMessage);
+        } catch (TelegramApiException e) {
+            logger.error("Error while sending start message: {}", e.getMessage());
+        }
+    }
 
-                SendMessage startMessage = new SendMessage();
-                startMessage.setChatId(message.getChatId().toString());
-                startMessage.setText("Привет, я бот для изучения английского языка. Выбери действие:");
-                // Создание клавиатуры
-                ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
-                markup.setResizeKeyboard(true);
-                markup.setOneTimeKeyboard(false);
-                List<KeyboardRow> keyboard = new ArrayList<>();
-                // Добавление кнопок
-                KeyboardRow row1 = new KeyboardRow();
-                KeyboardButton button_wordplay = new KeyboardButton();
-                button_wordplay.setText("Новое слово");
-                row1.add(button_wordplay);
-                KeyboardButton button_info = new KeyboardButton();
-                button_info.setText("О боте");
-                row1.add(button_info);
-                keyboard.add(row1);
-                markup.setKeyboard(keyboard);
-                startMessage.setReplyMarkup(markup);
-                try {
-                    execute(startMessage);
-                } catch (TelegramApiException e) {
-                    logger.error("Error while sending start message: {}", e.getMessage());
-                }
+    private void handleUnknownCommand(Message message) {
+        SendMessage unknownMessage = new SendMessage();
+        unknownMessage.setChatId(message.getChatId().toString());
+        unknownMessage.setText("Неизвестная команда");
+        try {
+            execute(unknownMessage);
+        } catch (TelegramApiException e) {
+            logger.error("Error while sending unknown command message: {}", e.getMessage());
+        }
     }
 
 }
