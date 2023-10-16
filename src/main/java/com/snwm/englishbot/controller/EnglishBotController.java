592ba2cd7f96c35cd5030e64e54104cc99b0d620
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-10-16 16:20:17 +0400

    controller change

--- src/main/java/com/snwm/englishbot/controller/EnglishBotController.java
+++ src/main/java/com/snwm/englishbot/controller/EnglishBotController.java
@@ -2,27 +2,15 @@ package com.snwm.englishbot.controller;
 
 import com.snwm.englishbot.entity.Word;
 import com.snwm.englishbot.service.WordService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EnglishBotController {
 
-    private final WordService wordService;
-
-    public EnglishBotController(WordService wordService) {
-        this.wordService = wordService;
-    }
-
-    @GetMapping("/getAllWords/{userId}")
-    public List<Word> getAllWordsByUser(@PathVariable(value = "userId") Long id) {
-        return wordService.getAllWordsByUser(id);
-    }
-
-    @GetMapping("/admin")
+    @GetMapping("/login")
     public String getAdminPage() {
-        return "admin";
+        return "login";
     }
 }
