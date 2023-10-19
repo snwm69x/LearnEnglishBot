eb3eec0a7159e44b424c3baeac93dde5c13f2af3
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-10-20 03:30:06 +0400

    disabled csrf

--- src/main/java/com/snwm/englishbot/config/SecurityConfig.java
+++ src/main/java/com/snwm/englishbot/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+        .csrf().disable()
             .authorizeRequests()
                 .antMatchers("/login/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
