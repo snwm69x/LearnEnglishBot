1fa6a080e8c8e9ab5137c82a92cdea5f6fffd7e6
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-11-07 04:31:06 +0400

    bug hunt v0.2

--- src/main/java/com/snwm/englishbot/bot/EnglishWordBot.java
+++ src/main/java/com/snwm/englishbot/bot/EnglishWordBot.java
@@ -315,6 +315,9 @@ public class EnglishWordBot extends TelegramLongPollingBot {
         options.add(word_translation);
         Collections.shuffle(options);
         String correctAnswer = options.get(options.indexOf(word_translation));
+        System.out.println("Word: " + word.getWord());
+        System.out.println("Word translation: " + word_translation);
+        System.out.println("Options: " + options);
         System.out.println("Size of correctAnswer: " + correctAnswer.getBytes().length);
         System.out.println("Size of options: " + options.toString().getBytes().length);
         System.out.println("Size of word.getId(): " + String.valueOf(word.getId()).getBytes().length);
@@ -353,6 +356,9 @@ public class EnglishWordBot extends TelegramLongPollingBot {
         options.add(word_name);
         Collections.shuffle(options);
         String correctAnswer = options.get(options.indexOf(word_name));
+        System.out.println("Word: " + word.getWord());
+        System.out.println("Word translation: " + word_name);
+        System.out.println("Options: " + options);
         System.out.println("Size of correctAnswer: " + correctAnswer.getBytes().length);
         System.out.println("Size of options: " + options.toString().getBytes().length);
         System.out.println("Size of word.getId(): " + String.valueOf(word.getId()).getBytes().length);
