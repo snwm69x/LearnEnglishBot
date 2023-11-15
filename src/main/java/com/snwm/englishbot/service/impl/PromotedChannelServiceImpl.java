9da23d999f0074c1cbf7f5416b1d7c291095d2e9
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-11-16 01:36:38 +0400

    Fix PromotedChannelServiceImpl update logic

--- src/main/java/com/snwm/englishbot/service/impl/PromotedChannelServiceImpl.java
+++ src/main/java/com/snwm/englishbot/service/impl/PromotedChannelServiceImpl.java
@@ -30,23 +30,22 @@ public class PromotedChannelServiceImpl implements PromotedChannelService {
         Optional<PromotedChannel> optionalPromotedChannel = promotedChannelRepository.findById(0L);
         if (optionalPromotedChannel.isPresent()) {
             PromotedChannel existingChannel = optionalPromotedChannel.get();
-
-            if (promotedChannel.getChannelDescription() != null) {
+            if (promotedChannel.getChannelDescription() != null
+                    && promotedChannel.getChannelDescription().equals(existingChannel.getChannelDescription())) {
                 existingChannel.setChannelDescription(promotedChannel.getChannelDescription());
             }
-
-            if (promotedChannel.getChannelLink() != null) {
+            if (promotedChannel.getChannelLink() != null
+                    && promotedChannel.getChannelLink().equals(existingChannel.getChannelLink())) {
                 existingChannel.setChannelLink(promotedChannel.getChannelLink());
             }
-
-            if (promotedChannel.getChannelName() != null) {
+            if (promotedChannel.getChannelName() != null
+                    && promotedChannel.getChannelName().equals(existingChannel.getChannelName())) {
                 existingChannel.setChannelName(promotedChannel.getChannelName());
             }
-
-            if (promotedChannel.getChatId() != null) {
+            if (promotedChannel.getChatId() != null
+                    && promotedChannel.getChatId().equals(existingChannel.getChatId())) {
                 existingChannel.setChatId(promotedChannel.getChatId());
             }
-
             promotedChannelRepository.save(existingChannel);
         } else {
             throw new RuntimeException("No promoted channel found to update");
