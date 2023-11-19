269bc1fbf0f73ff7d4246b9b487d313121b77ca1
snwm69x <119750978+snwm69x@users.noreply.github.com>
2023-11-19 19:49:25 +0400

    commented snwm69xbot

--- src/main/java/com/snwm/englishbot/bot/Snwm69xBot.java
+++ src/main/java/com/snwm/englishbot/bot/Snwm69xBot.java
@@ -5,31 +5,31 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-@Component
-public class Snwm69xBot extends TelegramLongPollingBot {
+// @Component
+// public class Snwm69xBot extends TelegramLongPollingBot {
 
-    private final String token;
-    private final String username;
+// private final String token;
+// private final String username;
 
-    Snwm69xBot(@Value("${SNWM_BOT_TOKEN}") String token,
-            @Value("${SNWM_BOT_NAME}") String username) {
-        this.token = token;
-        this.username = username;
-    }
+// Snwm69xBot(@Value("${SNWM_BOT_TOKEN}") String token,
+// @Value("${SNWM_BOT_NAME}") String username) {
+// this.token = token;
+// this.username = username;
+// }
 
-    @Override
-    public void onUpdateReceived(Update update) {
+// @Override
+// public void onUpdateReceived(Update update) {
 
-    }
+// }
 
-    @Override
-    public String getBotUsername() {
-        return this.username;
-    }
+// @Override
+// public String getBotUsername() {
+// return this.username;
+// }
 
-    @Override
-    public String getBotToken() {
-        return this.token;
-    }
+// @Override
+// public String getBotToken() {
+// return this.token;
+// }
 
-}
+// }
